5d409dc8fe545a6ee3161da11f4f5997b4fc4d74
Anna Kruglova <anya.kruglova.16@gmail.com>
2024-02-18 20:00:44 +0400

    домашка_2

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -7,7 +7,7 @@ from googleapiclient.discovery import build
 
 
 api_key: str = os.getenv('YT_API_KEY')
-print(api_key)
+
 
 # создать специальный объект для работы с API
 youtube = build('youtube', 'v3', developerKey=api_key)
--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,6 @@
 import json
 import os
-import isodate
+
 from googleapiclient.discovery import build
 
 class Channel:
@@ -8,10 +8,46 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
-        self.youtube = build('youtube', 'v3', developerKey=os.getenv('YT_API_KEY'))
-        self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.__channel_id = channel_id
+        self.youtube = build('youtube', 'v3', developerKey=os.getenv('API_KEY'))
+        self.channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.info_channel = json.dumps(self.channel, indent=2, ensure_ascii=False)
+        self.id = self.channel["items"][0]["id"]
+        self.title = self.channel["items"][0]["snippet"]["title"]
+        self.description = self.channel["items"][0]["snippet"]["description"]
+        self.subscriber_count = self.channel["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
+        self.view_count = self.channel["items"][0]["statistics"]["viewCount"]
+        self.url = f"https://www.youtube/channels/{self.__channel_id}"
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        print(json.dumps(self.channel,indent=2, ensure_ascii=False))
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @channel_id.setter
+    def channel_id(self, new_chann_id):
+        if new_chann_id:
+            print("AttributeError: property 'channel_id' of 'Channel' object has no setter")
+            self.__channel_id = self.__channel_id
+
+    @classmethod
+    def get_service(cls):
+        youtube = build('youtube', 'v3', developerKey=os.getenv('API_KEY'))
+        return youtube
+
+    def to_json(self, file_json):
+        json_data = {
+            'channel_id': self.__channel_id,
+            'title': self.title,
+            'descrition': self.description,
+            'url': self.url,
+            'subscriber_count': self.subscriber_count,
+            'videoCount': self.video_count,
+            'viewCount': self.view_count
+        }
+        with open(file_json, 'w', encoding="utf-8") as f:
+            f.write(json.dumps(json_data, ensure_ascii=False, indent=4))
